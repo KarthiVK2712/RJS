8a15afc91cd001b0daf78d4ee8c443296fc73773
Karthi V K <santhikarthi0@gmail.com>
2023-08-03 16:46:20 +0530

    changed from local to remote

---
+++ JS/Sample.js
@@ -0,0 +1,27 @@
+// var a = 120;
+// console.log("this is the value declared in var type: "+a);
+// let b = 50;
+// console.log("this is the value declared in let type: "+b);
+// const c = 240;
+// console.log(`this is the value declared in const type: ${c}`);
+// var d = 20;
+// console.log("this is the value declared in var type before altering: "+d);
+// {
+//     var d = 7;
+//     console.log(`this is the value declared in var type after altering: ${d}`);
+// }
+// console.log("this is the value declared in var type after block altering: "+d);
+// // const c = 240;
+// console.log(`this is the value declared in const type: ${c}`);
+// c = 5
+// console.log("this is the value declared in const type: "+c);
+a = -20
+if(a>=0){
+    console.log("The number is Positive",a)
+}
+else if(a==0){
+    console.log("The number is Zero")
+}
+else{
+    console.log("The number is Negative")
+}
